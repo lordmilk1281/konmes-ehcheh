0c32f06afe5b10e92741ca363721574e9105e404
princess abu <abhaaibrahim@gmail.com>
2023-10-29 10:30:38 +0500

    responsive ness

--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -4,7 +4,7 @@ type Props = {};
 
 const AboutUs = (props: Props) => {
   return (
-    <div className="mx-auto container space-y-10 my-10">
+    <div className="mx-auto container space-y-10 my-10 px-4">
       <Card
         title="Mission"
         description="The vision and mission of the Institute of Qur'an and Sunnah are deeply rooted in the core philosophy of the institution, encapsulated by the guiding principle of 'Kitabullah wa Sunnati' (The Book of Allah and the Sunnah of My Prophet). This fundamental philosophy serves as the bedrock upon which the entire institution is founded, shaping its overarching goals, values, and aspirations."
@@ -26,7 +26,7 @@ type CardProps = {
 
 const Card = ({ title, description }: CardProps) => {
   return (
-    <section className="border-2 p-4 w-1/2 rounded-lg border-gray-100 shadow-sm bg-gray-50 mx-auto">
+    <section className="border-2 p-4 sm:w-1/2 rounded-lg border-gray-100 shadow-sm bg-gray-50 mx-auto">
       <p className="font-bold text-xl md:text-3xl text-gray-900">{title}</p>
       <p className="font-medium text-gray-700 text-md mt-2">
         {description}
--- app/courses/page.tsx
+++ app/courses/page.tsx
@@ -27,7 +27,7 @@ const mainGroups = [
 
 const Courses = (props: Props) => {
   return (
-    <div className="my-10 mx-auto container space-y-5">
+    <div className="my-10 mx-auto container space-y-5 px-4">
       <section>
         <h1 className="font-bold text-xl md:text-3xl text-gray-900">Courses</h1>
         <p className="font-medium text-gray-700 text-md">
@@ -36,11 +36,13 @@ const Courses = (props: Props) => {
           focus to a particular course.
         </p>
       </section>
-      <section className="grid grid-cols-3 gap-6">
+      <section className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
         {mainGroups.map(group => (
           <div key={group.name} className="space-y-1 group cursor-pointer">
             <div className="bg-emerald-300 rounded-lg aspect-video group-hover:bg-emerald-400" />
-            <p className="font-medium text-md md:text-xl text-gray-700 group-hover:text-gray-900">{group.name}</p>
+            <p className="font-medium text-lg md:text-xl text-gray-700 group-hover:text-gray-900">
+              {group.name}
+            </p>
           </div>
         ))}
       </section>
